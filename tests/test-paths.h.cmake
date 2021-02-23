e1ebc6c1e244c24fac47f61560b20004149e36a2
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-02-23 15:19:15 +0100

    Rebase from 'release/noetic/sot-core'

---
+++ tests/test-paths.h.cmake
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2010,
+ * François Bleibel,
+ * Olivier Stasse,
+ *
+ * CNRS/AIST
+ *
+ */
+
+#ifndef TEST_PATHS_H
+
+#define PLUGIN_LIB_INSTALL_PATH "${SOT_PLUGIN_OUT_DIR}"
+
+#endif
